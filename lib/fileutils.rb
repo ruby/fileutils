ecef11bf2c396eb66af1bca471b2d26917c6e6ff
BurdetteLamar <burdettelamar@yahoo.com>
2022-06-16 08:08:25 -0700

    Revisions for module-level doc

--- lib/fileutils.rb
+++ lib/fileutils.rb
@@ -6,17 +6,7 @@ rescue LoadError
   # for make mjit-headers
 end
 
-#
-# = fileutils.rb
-#
-# Copyright (c) 2000-2007 Minero Aoki
-#
-# This program is free software.
-# You can distribute/modify this program under the same terms of ruby.
-#
-# == module FileUtils
-#
-# Namespace for several file utility methods for copying, moving, removing, etc.
+# Namespace for file utility methods for copying, moving, removing, etc.
 #
 # == Path Arguments
 #
@@ -59,88 +49,41 @@ end
 #       |-- find_executable.rb
 #       `-- helper.rb
 #
-# === Module Functions
+# == Options
 #
-#   require 'fileutils'
+# Options to the methods are given as keyword arguments.
 #
-#   FileUtils.cd(dir, **options)
-#   FileUtils.cd(dir, **options) {|dir| block }
-#   FileUtils.pwd()
-#   FileUtils.mkdir(dir, **options)
-#   FileUtils.mkdir(list, **options)
-#   FileUtils.mkdir_p(dir, **options)
-#   FileUtils.mkdir_p(list, **options)
-#   FileUtils.rmdir(dir, **options)
-#   FileUtils.rmdir(list, **options)
-#   FileUtils.ln(target, link, **options)
-#   FileUtils.ln(targets, dir, **options)
-#   FileUtils.ln_s(target, link, **options)
-#   FileUtils.ln_s(targets, dir, **options)
-#   FileUtils.ln_sf(target, link, **options)
-#   FileUtils.cp(src, dest, **options)
-#   FileUtils.cp(list, dir, **options)
-#   FileUtils.cp_r(src, dest, **options)
-#   FileUtils.cp_r(list, dir, **options)
-#   FileUtils.mv(src, dest, **options)
-#   FileUtils.mv(list, dir, **options)
-#   FileUtils.rm(list, **options)
-#   FileUtils.rm_r(list, **options)
-#   FileUtils.rm_rf(list, **options)
-#   FileUtils.install(src, dest, **options)
-#   FileUtils.chmod(mode, list, **options)
-#   FileUtils.chmod_R(mode, list, **options)
-#   FileUtils.chown(user, group, list, **options)
-#   FileUtils.chown_R(user, group, list, **options)
-#   FileUtils.touch(list, **options)
+# The most common options (often useful together) are:
 #
-# Possible <tt>options</tt> are:
+# - +noop+: Do not actually make modifications.
+# - +verbose+: Print a command that is equivalent to the method
+#   called with the given arguments and options.
 #
-# <tt>:force</tt> :: forced operation (rewrite files if exist, remove
-#                    directories if not empty, etc.);
-# <tt>:verbose</tt> :: print command to be run, in bash syntax, before
-#                      performing it;
-# <tt>:preserve</tt> :: preserve object's group, user and modification
-#                       time on copying;
-# <tt>:noop</tt> :: no changes are made (usable in combination with
-#                   <tt>:verbose</tt> which will print the command to run)
+# Other options include:
 #
-# Each method documents the options that it honours. See also ::commands,
-# ::options and ::options_of methods to introspect which command have which
-# options.
+# - +dereference_root+: Whether, if the argument is a symbolic, to follow it.
+# - +force+: Whether to force the operation
+#   (overwrite entries, remove non-empty directory, etc.).
+# - +group+: Sets an entry's group.
+# - +mode+: Sets an entry's permissions.
+# - +mtime+: Sets an entry's modification time.
+# - +nocreate+: Whether to raise an exception if an entry does not exist
+#   (rather than creating the entry).
+# - +owner+: Sets an entry's owner.
+# - +parents+: Whether to remove empty ancestor directories.
+# - +preserve+: Whether to preserve an entry's group, user, and modification time
+#   when copying.
+# - +remove_destination+: Whether to remove the destination entry before copying
+#   to it.
+# - +secure+: Whether to remove the source entry securely.
 #
-# All methods that have the concept of a "source" file or directory can take
-# either one file or a list of files in that argument.  See the method
-# documentation for examples.
+# See also:
 #
-# There are some `low level' methods, which do not accept keyword arguments:
-#
-#   FileUtils.copy_entry(src, dest, preserve = false, dereference_root = false, remove_destination = false)
-#   FileUtils.copy_file(src, dest, preserve = false, dereference = true)
-#   FileUtils.copy_stream(srcstream, deststream)
-#   FileUtils.remove_entry(path, force = false)
-#   FileUtils.remove_entry_secure(path, force = false)
-#   FileUtils.remove_file(path, force = false)
-#   FileUtils.compare_file(path_a, path_b)
-#   FileUtils.compare_stream(stream_a, stream_b)
-#   FileUtils.uptodate?(file, cmp_list)
-#
-# == module FileUtils::Verbose
-#
-# This module has all methods of FileUtils module, but it outputs messages
-# before acting.  This equates to passing the <tt>:verbose</tt> flag to methods
-# in FileUtils.
-#
-# == module FileUtils::NoWrite
-#
-# This module has all methods of FileUtils module, but never changes
-# files/directories.  This equates to passing the <tt>:noop</tt> flag to methods
-# in FileUtils.
-#
-# == module FileUtils::DryRun
-#
-# This module has all methods of FileUtils module, but never changes
-# files/directories.  This equates to passing the <tt>:noop</tt> and
-# <tt>:verbose</tt> flags to methods in FileUtils.
+# - ::commands: Returns an array of the string names of methods
+#   that accept keyword options.
+# - ::have_option?: Returns whether the given method accepts the given option.
+# - ::options: Returns an array of the option names.
+# - ::options_of: Returns an array of the option-names that the given method accepts.
 #
 # == Avoiding the TOCTTOU Vulnerability
 #
@@ -2411,7 +2354,7 @@ module FileUtils
   public
 
   # Returns an array of the string names of \FileUtils methods
-  # that accept one or more keyword arguments;
+  # that accept one or more keyword arguments:
   #
   #   FileUtils.commands.sort.take(3) # => ["cd", "chdir", "chmod"]
   #
